8da559bcacc64400e3dfd8ed7f885fce14b3221f
Caroline Brum <carolinefbrum@gmail.com>
2022-08-16 14:42:16 -0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Read all tasks
+  get 'tasks', to: 'tasks#index'
+
+  # Create a restaurant
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # Read one task details
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # Update/Edit a restaurant
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Delete a restaurant
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
